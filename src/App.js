202a4f6c50b62b94e55a7fa9b2a55c2c0eba4d45
Schattsch <karelsch123@gmail.com>
2023-02-01 16:20:13 +0200

    Baas failid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+
 import './App.css';
 
-function App() {
-  return (
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        date: new Date(2024,0,10),
+        title: 'New book',
+        amount: 30.99
+    },
+    {
+        id: 'e2',
+        date: new Date(2024,0,10),
+        title: 'New jeans',
+        amount: 99.99
+    },
+    {
+        id: 'e3',
+        date: new Date(2023,0,18),
+        title: 'New bag',
+        amount: 199.99
+    }
+]
+const App = () => {
+    const [expenses, setExpenses] = useState (DUMMY_EXPENSES)
+    const [filteredYear, setFilteredYear] = useState('2023')
+    const filterChangeHandler = (year) => {
+        setFilteredYear(year)
+    }
+
+    const addExpensehandler = (expense) => {
+        console.log('In App.js')
+        setExpenses((previousExpenses) => {
+            return [expense, ...previousExpenses]
+        })
+    }
+    console.log(expenses)
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NewExpense onAddExpense={addExpensehandler}></NewExpense>
+        <Expenses expenses={expenses}></Expenses>
     </div>
   );
 }
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './Expenses.css'
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter"
+import ExpensesList from "./ExpensesList";
+
+
+
+function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2023')
+    console.log('Year data in Expenses.js' + filteredYear)
+
+    const filterChangeHandler = (year) => {
+        console.log('Filter change handled by Expenses.js')
+        console.log(year + ' in Expenses.js')
+        setFilteredYear(year)
+    }
+
+    const filteredExpenses = props.expenses.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
+
+
+
+
+    return (
+        <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
+            <ExpensesList filteredExpenses={filteredExpenses}></ExpensesList>
+        </Card>
+    )
+}
+
+export default Expenses
