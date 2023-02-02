7d1295ca8a2a73097f60612e8a908997de787f4c
Schattsch <karelsch123@gmail.com>
2023-02-02 13:38:25 +0200

    Üldine CSS-i korrastamine

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,59 +2,60 @@ import React, { useState } from 'react';
 import './ExpenseForm.css'
 const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('')
-    const [enteredAmount, setEnteredAmount] = useState('')
     const [enteredDate, setEnteredDate] = useState('')
-    const [formEdit, setFormEdit] = useState(false);
+    const [enteredPriority, setEnteredPriority] = useState('')
 
     const titleChangeHandler = (event) => {
-            setEnteredTitle(event.target.value)
-        }
-
-    const amountChangeHandler = (event) => {
-            setEnteredAmount(event.target.value)
-        }
+        setEnteredTitle(event.target.value)
+    }
 
     const dateChangeHandler = (event) => {
-            setEnteredDate(event.target.value)
-        }
+        setEnteredDate(event.target.value)
+    }
 
-        const submitHandler = (event) => {
+    const priorityChangeHandler = (event) => {
+        setEnteredPriority(event.target.value)
+    }
+
+    const submitHandler = (event) => {
         event.preventDefault()
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
+            date: new Date(enteredDate),
+            priority: enteredPriority
         }
         props.onSaveExpenseData(expenseData)
-            setEnteredTitle('')
-            setEnteredAmount('')
-            setEnteredDate('')
-            setFormEdit(false)
-        }
+        setEnteredTitle('')
+        setEnteredDate('')
+        setEnteredPriority('')
+    }
 
-
-return (
-    <form onSubmit={submitHandler}>
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>Title</label>
-                <input type="text"
-                       onChange={titleChangeHandler} value={enteredTitle}/>
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>New Task</label>
+                    <input type="text"
+                           onChange={titleChangeHandler} value={enteredTitle}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" min="2023-01-18" max="2025-12-31"
+                           onChange={dateChangeHandler} value={enteredDate}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Select task priority</label>
+                    <select onChange={priorityChangeHandler} value={enteredPriority}>
+                        <option value="low">Low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                    </select>
+                </div>
             </div>
-            <div className="new-expense__control">
-                <label>Amount</label>
-                <input type="number" min="1" step="1" onChange={amountChangeHandler} value={enteredAmount}/>
+            <div className="new-expense__actions">
+                <button type="submit">Add Task</button>
             </div>
-            <div className="new-expense__control">
-                <label>Date</label>
-                <input type="date" min="2023-01-18" max="2025-12-31"
-                       onChange={dateChangeHandler} value={enteredDate}/>
-            </div>
-        </div>
-        <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
-        </div>
-    </form>
-)
+        </form>
+    )
 }
 export default ExpenseForm
